9c0b030730a11cc9474e96c51f4ad077e8b942b3
Nishant Shukla <114081634+NishantShukla10@users.noreply.github.com>
2024-09-14 18:08:02 +0530

    Update helper.js

--- client/src/services/helper.js
+++ client/src/services/helper.js
@@ -1,2 +1,4 @@
-export const BASE_URL = "http://localhost:6010"
+// export const BASE_URL = "http://localhost:6010"
 // export const BASE_URL = "https://mernappbackend.onrender.com"
+
+export const BASE_URL = "https://user-app-details-backend.onrender.com"
